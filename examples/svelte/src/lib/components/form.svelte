aae460bfb621029d98bb12c3176fc293264e876e
Sahin Vardar <sahin@vardar.io>
2025-03-04 08:44:31 +0100

    feat: feat better schema handling (#19)

--- examples/svelte/src/lib/components/form.svelte
+++ examples/svelte/src/lib/components/form.svelte
@@ -5,22 +5,40 @@
     setValidationErrorsToForm,
     setFormDataToForm,
     objectToFormData,
-    clearFormValidationErrors    
+    clearFormValidationErrors,
+    formDataToObject
   } from '@vardario/zod-form-validation';
 </script>
 
 <script lang="ts">
+  import { onMount } from 'svelte';
+
   import type { ZodAny, AnyZodObject, z, ZodEffects } from 'zod';
   import type { PartialDeep } from 'type-fest';
 
   type T = $$Generic<AnyZodObject | ZodAny | ZodEffects<AnyZodObject>>;
   type ObjectType = z.infer<typeof schema>;
+  let slotContainer: HTMLDivElement | undefined = undefined;
 
   export let schema: T;
   export let data: PartialDeep<ObjectType> | undefined = undefined;
+  export let onChange: (data: PartialDeep<ObjectType>) => void = () => {};
 
   let form: HTMLFormElement;
   let doValidateOnInput = false;
+
+  onMount(() => {
+    const observer = new MutationObserver(() => {
+      setRequiresToForm(form, schema);
+    });
+    if (slotContainer) {
+      observer.observe(slotContainer, { childList: true, subtree: true });
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  });
 
   $: {
     form && data && setFormDataToForm(form, data);
@@ -33,7 +51,18 @@
     form = _form;
   }
 
+  function _onChange() {
+    setRequiresToForm(form, schema);
+    onChange(formDataToObject(new FormData(form), schema));
+
+    if (doValidateOnInput) {
+      validateFormData(new FormData(form), schema);
+    }
+  }
+
   function fromValidation() {
+    onChange(formDataToObject(new FormData(form), schema));
+
     const result = validateFormData(new FormData(form), schema);
 
     if (result.success === false) {
@@ -48,7 +77,7 @@
 <form
   novalidate
   use:enhance
-  on:input={() => doValidateOnInput && fromValidation()}
+  on:input={_onChange}
   on:submit
   on:submit={event => {
     doValidateOnInput = true;
@@ -59,5 +88,7 @@
   }}
   {...$$props}
 >
-  <slot />
+  <div bind:this={slotContainer}>
+    <slot />
+  </div>
 </form>
